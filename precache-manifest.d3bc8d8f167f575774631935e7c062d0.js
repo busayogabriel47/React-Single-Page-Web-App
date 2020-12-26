7b566936db5a84f45c55a35919b472fa91412213
busayogabriel47 <47152329+busayogabriel47@users.noreply.github.com>
2020-12-26 10:50:53 +0100

    Updates

---
+++ precache-manifest.d3bc8d8f167f575774631935e7c062d0.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b75fa23f87b9e6a303274eb634fbcc58",
+    "url": "/React-Single-Page-Web-App/index.html"
+  },
+  {
+    "revision": "4399a98185361452880b",
+    "url": "/React-Single-Page-Web-App/static/css/2.ee406fcf.chunk.css"
+  },
+  {
+    "revision": "80defbc5e6e57aebcc09",
+    "url": "/React-Single-Page-Web-App/static/css/main.540c153e.chunk.css"
+  },
+  {
+    "revision": "4399a98185361452880b",
+    "url": "/React-Single-Page-Web-App/static/js/2.2eb1d613.chunk.js"
+  },
+  {
+    "revision": "293f44f1d92634ddda655bf639d9ea59",
+    "url": "/React-Single-Page-Web-App/static/js/2.2eb1d613.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "80defbc5e6e57aebcc09",
+    "url": "/React-Single-Page-Web-App/static/js/main.a7c307dc.chunk.js"
+  },
+  {
+    "revision": "519b506b9a6b17a81033",
+    "url": "/React-Single-Page-Web-App/static/js/runtime-main.9fcf42e1.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/React-Single-Page-Web-App/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/React-Single-Page-Web-App/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/React-Single-Page-Web-App/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/React-Single-Page-Web-App/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/React-Single-Page-Web-App/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
